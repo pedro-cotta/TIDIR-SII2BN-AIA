78ff03f18d3c397ddfbc9c11b7eff0d4f9dc870b
João Grabriel <joao.rodrigues@iof.mg.gov.br>
2015-08-12 15:14:00 -0300

    pontos e markerclusterer

--- application/views/mapa.php
+++ application/views/mapa.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,12 +10,13 @@
 	<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 </head>
 <body>		
-	<?php $this->load->view('nav');?>
+<?php $this->load->view('nav');?>
 	<div class="container">
 		<div id="formularioMapa" class="row">
 			<div id="mapa" class="col-md-12 well"></div>
 		</div>
 	</div>
+	<script src="js/markerclusterer.js"></script>
 	<script src="js/infobox.js"></script>
 	<script src="<?php echo base_url ("js/jquery.min.js");?>"></script>
 	<script src="<?php echo base_url ("js/bootstrap.min.js");?>"></script>
@@ -26,6 +26,7 @@
 
 		var idInfoBoxAberto;
 		var infoBox = [];
+		var markers = [];
 
 		function abrirInfoBox(id, marker) {
 			if (typeof(idInfoBoxAberto) == 'string' && typeof(infoBox[idInfoBoxAberto]) == 'object') 
@@ -35,12 +36,13 @@
 
 			infoBox[id].open(map, marker);
 			idInfoBoxAberto = id;
-			console.log(typeof(idInfoBoxAberto));
-			console.log(typeof(infoBox[idInfoBoxAberto]));
 		}
 
 		function carregarPontos() {
 			$.getJSON("<?php echo base_url('index.php/mapa/pegaPontos')?>", function(pontos) {
+				
+				var latlngbounds = new google.maps.LatLngBounds();
+
 				$.each(pontos, function(index, ponto) {
 					var marker = new google.maps.Marker({
 						position: new google.maps.LatLng(ponto.latitude, ponto.longitude),
@@ -50,7 +52,7 @@
 					});
 
 					var myOptions = {
-						content:"<h4 class='text-center text-uppercase'>"+ponto.nome+"</h4>"+' '+"<p>"+ponto.descricao+"</p>",
+						content:"<h5 class='text-center text-uppercase'>"+ponto.nome+"</h5>"+' '+"<p>"+ponto.descricao+"</p>",
 						pixelOffset: new google.maps.Size(-150, 0)
 					};
 
@@ -61,33 +63,17 @@
 						abrirInfoBox(ponto.id, marker);
 					});
 
+					markers.push(marker);
+					latlngbounds.extend(marker.position);
+
 				});
+
+				var markerCluster = new MarkerClusterer(map, markers);
+				map.fitBounds(latlngbounds);
+
 			});
 		}
 		carregarPontos();
-
-		/*$.ajax({
-			url: "<?php echo base_url('index.php/mapa/pegaPontos')?>",
-			dataType : 'json',
-			success: function(data){
-				for (var i = 0; i < data.length; i++) {
-					coord = new google.maps.LatLng(data[i].latitude, data[i].longitude);
-					console.log(data[i].latitude + ' '+data[i].longitude);
-					marker = new google.maps.Marker({
-					//icon: 'marker.png',
-					position: coord,
-					map: map
-				});
-					var infowindow = new google.maps.InfoWindow(), marker;
-					google.maps.event.addListener(marker, 'click', (function(marker, i) {
-						return function() {
-							infowindow.setContent("Conteúdo do marcador.");
-							infowindow.open(map, marker);
-						}
-					})(marker))
-				}
-			}
-		});*/
 </script>
 </body>
 </html>
--- application/views/nav.php
+++ application/views/nav.php
@@ -1,26 +1,27 @@
-	<nav class="navbar navbar-default">
-		<div class="container">
-			<div class="navbar-header">
-				<button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
-					<span class="sr-only">Navegação</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-			</div>
-			<div class="navbar-collapse collapse" id="navbar">
-				<ul class="nav navbar-nav">
-					<li><a class="navbar-brand" href="#">EasyPark</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-cog"> </span><span class="caret"></span></a>
-						<ul class="dropdown-menu dropdown-menu-left" role="menu">
-							<li><?=anchor("","Sair");?></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
+
+<nav class="navbar navbar-default">
+
+		<div class="navbar-header">
+			<button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+				<span class="sr-only">Navegação</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 		</div>
-	</nav>
+		<div class="navbar-collapse collapse" id="navbar">
+			<ul class="nav navbar-nav">
+				<li><a class="navbar-brand" href="#">EasyPark</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-cog"> </span><span class="caret"></span></a>
+					<ul class="dropdown-menu dropdown-menu-left" role="menu">
+						<li><?=anchor("","Sair");?></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	
+</nav>
