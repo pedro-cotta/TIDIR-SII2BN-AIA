b4ab8d244b4bad707574ded6c13758c759476477
João Grabriel <joao.rodrigues@iof.mg.gov.br>
2015-08-31 13:47:25 -0300

    Posicao Arrumada

--- js/mapa.js
+++ js/mapa.js
@@ -1,18 +1,32 @@
 'use strict'
 var map;
 var markerInicial;
-var posicao = new Array;
+var latlng;
+var options;
 
 function initialize() {
 
-	var latlng = new google.maps.LatLng(-19.923962, -43.938712);
+	console.log(latlng);
+
 	var options = {
 		zoom: 16,
-		center: latlng,
+		center: {lat: -19.918534, lng: -43.941391},
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	map = new google.maps.Map(document.getElementById("mapa"), options);
+
 	var geocoder = new google.maps.Geocoder();
+
+	map = new google.maps.Map(document.getElementById("mapa"), options);
+
+	navigator.geolocation.getCurrentPosition(function(position) {
+		var pos = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		};
+
+		markerInicial.setPosition(pos);
+		map.setCenter(pos);
+	});
 
 	markerInicial = new google.maps.Marker({
 		map: map,
@@ -21,12 +35,5 @@ function initialize() {
 		title: 'Posição Atual',
 		animation:  google.maps.Animation.BOUNCE
 	});
-
-	markerInicial.setPosition(navigator.geolocation.getCurrentPosition());
-	/*navigator.geolocation.getCurrentPosition(function(position){
-		markerInicial.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-	});*/
-//markerInicial.setPosition(latlng);
-
 }
 initialize();
