d859f822f27508c93912ecea62c0b08b55f5924e
João Grabriel <joao.rodrigues@iof.mg.gov.br>
2015-08-31 11:54:36 -0300

    juncao git commit -m juncao
    ;
    .
    git commit -m juncao

--- js/mapa.js
+++ js/mapa.js
@@ -5,52 +5,27 @@ var posicao = new Array;
 
 function initialize() {
 
-try {
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position){
-			markerInicial.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-			posicao.push(position.coords.latitude);
-			posicao.push(position.coords.longitude);
-			console.log(posicao);
-		}, 
-		function(error){
-			alert('Erro ao obter localização!');
-			console.log('Erro ao obter localização.', error);
-		});
+	var latlng = new google.maps.LatLng(-19.923962, -43.938712);
+	var options = {
+		zoom: 16,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("mapa"), options);
+	var geocoder = new google.maps.Geocoder();
 
-		
-		var latlng = markerInicial; // new google.maps.LatLng(-19.9166813,-43.9344931);
-
-
-	} else {
-		console.log('Navegador não suporta Geolocalização!');
-	}
-
-		navigator.geolocation.getCurrentPosition(function(pos){
-		var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-
-		var options = {
-			zoom: 16,
-			center: latlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		map = new google.maps.Map(document.getElementById("mapa"), options);
-		var geocoder = new google.maps.Geocoder();
-
-			markerInicial = new google.maps.Marker({
-			map: map,
-			draggable: false,
-			icon: 'img/markerInitial2.png',
-			title: 'Posição Atual',
-			animation:  google.maps.Animation.BOUNCE
-		});
-
+	markerInicial = new google.maps.Marker({
+		map: map,
+		draggable: false,
+		icon: 'img/markerInitial2.png',
+		title: 'Posição Atual',
+		animation:  google.maps.Animation.BOUNCE
 	});
-	}
-catch(err) {
-    console.log("ERRO PORRA");
-}
 
+	markerInicial.setPosition(navigator.geolocation.getCurrentPosition());
+	/*navigator.geolocation.getCurrentPosition(function(position){
+		markerInicial.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+	});*/
 //markerInicial.setPosition(latlng);
 
 }
